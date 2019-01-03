19b7dad006d3683fd94371141a993984c12f662b
Filicio <filicio.david@etec.sp.gov.br>
2019-01-03 11:57:50 -0200

    Update welcome.blade.php
    alteração homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Escala</title>
 </head>
 <body>
-   <?php
-   for($i = 0;$i <10 ;$i++){
-   $int = rand(1,35);
-    if ($int == $int ){
-        $int = rand(1,35);
-        echo $int;
-    }else{
-        echo $int;  
-    }
-   
-   };?>
+  <h1>Espaço para a tabela de escalass</h1>
 </body>
 </html>
