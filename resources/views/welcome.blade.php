35723d82197a6e94461ebd3003580c3228654968
Filicio <filicio.david@etec.sp.gov.br>
2019-01-03 11:47:13 -0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   for($i = 0;$i <10 ;$i++){
+   $int = rand(1,35);
+    if ($int == $int ){
+        $int = rand(1,35);
+        echo $int;
+    }else{
+        echo $int;  
+    }
+   
+   };?>
+</body>
+</html>
