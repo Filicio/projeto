0aac58f9d24cb13fce6688d9550975abcd8a3d7b
Filicio <filicio.david@etec.sp.gov.br>
2019-01-03 15:00:42 -0200

    Correções de Bugs

--- database/migrations/2019_01_03_145827_create_celebs_table.php
+++ database/migrations/2019_01_03_145827_create_celebs_table.php
@@ -15,7 +15,8 @@ class CreateCelebsTable extends Migration
     {
         Schema::create('celebs', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->date("data");
+            $table->string("tipo");
         });
     }
 
